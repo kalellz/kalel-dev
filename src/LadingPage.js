3714d58b319e8b24db941433b13c33fc4b076911
Kalel <ZoroPlay1220@gmail.com>
2025-02-24 21:12:36 -0300

    starting

---
+++ src/LadingPage.js
@@ -0,0 +1,18 @@
+import './LandingPage.scss';
+
+function App() {
+  return (
+    <main className='Main'>
+      <header className='Header'>
+
+      </header>
+      <body className='Body'>
+
+      </body>
+      <footer className='Footer'>
+
+      </footer>
+    </main>
+  );
+}
+export default App;
